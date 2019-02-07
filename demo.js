9afce87d724262c523f144e34739010677ee997b
victormuzi <373969304@qq.com>
2019-02-07 13:08:51 +0800

    修改demo.js的内容

--- demo.js
+++ demo.js
@@ -9,8 +9,8 @@ process.chdir("./" + dirName)
 fs.mkdirSync('css')
 fs.mkdirSync('js')
 
-fs.writeFileSync("./index.html","<!DOCTYPE>")
-fs.writeFileSync("./css/style.css","h1{color: red;}")
-fs.writeFileSync("./js/main.js","var")
+fs.writeFileSync("./index.html","<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>");
+fs.writeFileSync("./css/style.css","h1{color: red;}");
+fs.writeFileSync("./js/main.js",'var string = "Hello World"\nalert(string)');
 
 process.exit(0)
